f17de3c16d832bfe3b8dca76cd84e27b7b78b3bb
STAKEWORLD <info@stakeworld.nl>
2023-04-12 11:16:41 +0000

    Rewrite logic

--- node-install.sh
+++ node-install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-# STAKEWORLD 2022
+# STAKEWORLD 2023
 # Install a node
-
-# Error handling
-error() {
-    echo "Error on line $1"
-    echo "Exiting"
-    exit 1
-}
-
-trap 'error $LINENO' ERR
 
 # Init 
 if ! which whiptail > /dev/null; then
@@ -20,21 +11,28 @@ if ! which whiptail > /dev/null; then
    fi
 fi
 
-export NEWT_COLORS='root=,blue'
-
 # Functions
+export NEWT_COLORS='root=,blue'
 
 function menu() { whiptail --menu "$@" 3>&1 1>&2 2>&3 ; }
 function msg() { whiptail --msgbox "$@" 0 75; }
 function input() { whiptail --inputbox "$@" 3>&1 1>&2 2>&3 ; }
 function yesno() { whiptail --yesno "$@" 0 70 3>&1 1>&2 2>&3 ; }
 
-function mainmenu() { 
-	action=$(menu "STAKEWORLD polkadot/kusama node installer \n\nThis script can install a node, the startup scripts, firewall and optionally restore a snapshot. The installer creates a script which you can review before executing." 0 75 0 nodeinstall "Install a node" snapinstall "Restore a snapshot" "exit" "Exit installer") 
-	$action
-}
 
-function nodeinstall {
+# Init script
+scriptdir="/tmp/node-install"
+if [ -d $scriptdir ] ; then
+	mv /tmp/node-install /tmp/node-install.$(date +%F.%R)
+fi
+	
+mkdir -p $scriptdir
+touch $scriptdir/install.sh
+chmod +x $scriptdir/install.sh
+touch $scriptdir/snapshot.sh
+chmod +x $scriptdir/snapshot.sh
+servername=`hostname`
+date=`date`
 
 # Start our script
 cat << EOF >> $scriptdir/install.sh
@@ -43,54 +41,7 @@ cat << EOF >> $scriptdir/install.sh
 
 EOF
 
-chain=$(menu "Which chain do you want to install" 0 75 0 polkadot "polkadot node" "kusama" "kusama node" )
-database=$(menu "Which database do you want to install" 0 75 0 paritydb "Paritydb (newer, quicker)" "rocksdb" "Rocksdb (more stable)" )
-#database="paritydb"
-telemetry=$(menu "Which telemetry to use" 0 75 0 "telemetry.polkadot.io" "default telemetry" "telemetry-backend.w3f.community" "1000 validator telemetry" )
-nodedir=$(menu "Where do you want to install" 0 75 0 "/home/polkadot" " best for multi-node install" )
-nodename=$(input "How do you want to name your node. This will be used in the systemctl script and in the homedirectory." 0 70 "mynode" )
-nodenumber=$(menu "Depending on hardware and network it is possible to install extra nodes, official advise is one node per server. For one node choose 01, for the second 02, etc" 0 75 0 "01" "first node" "02" "Second node" "03" "Third node" )
-
-cat << EOF >> $scriptdir/$nodename-$nodenumber.service
-[Unit]
-Description=$chain $nodename-$nodenumber
-After=network.target
-Documentation=https://github.com/paritytech/polkadot
-StartLimitIntervalSec=200
-StartLimitBurst=2
-
-[Service]
-ExecStart=/usr/bin/polkadot --chain $chain --name $nodename-$nodenumber --validator --state-pruning 1000 --blocks-pruning 1000 --port 303$nodenumber --rpc-port 99$nodenumber --ws-port 98$nodenumber --prometheus-port 96$nodenumber --prometheus-external --base-path $nodedir/$nodename-$nodenumber --database $database --telemetry-url 'wss://$telemetry/submit 1' 
-User=polkadot
-Group=polkadot
-Restart=always
-RestartSec=30
-CapabilityBoundingSet=
-LockPersonality=true
-NoNewPrivileges=true
-PrivateDevices=true
-PrivateMounts=true
-PrivateTmp=true
-PrivateUsers=true
-ProtectClock=true
-ProtectControlGroups=true
-ProtectHostname=true
-ProtectKernelModules=true
-ProtectKernelTunables=true
-ProtectSystem=strict
-RemoveIPC=true
-RestrictAddressFamilies=AF_INET AF_INET6 AF_NETLINK AF_UNIX
-RestrictNamespaces=true
-RestrictSUIDSGID=true
-SystemCallArchitectures=native
-SystemCallFilter=@system-service
-SystemCallFilter=~@clock @module @mount @reboot @swap @privileged
-UMask=0027
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
+# Install binary
 if (yesno "You want to install the polkadot binary via apt?"); then
 cat << EOF >> $scriptdir/install.sh
 # Import the security@parity.io GPG key
@@ -106,19 +57,7 @@ apt -y install polkadot
 EOF
 fi
 
-cat << EOF >> $scriptdir/install.sh
-# Install systemctl script
-cp $scriptdir/$nodename-$nodenumber.service /etc/systemd/system
-# Start/stop polkadot to create directory structure
-echo "Starting the polkadot process and waiting 5 seconds for creation of the directory"
-systemctl daemon-reload
-systemctl start $nodename-$nodenumber
-sleep 5
-systemctl stop $nodename-$nodenumber
-systemctl enable $nodename-$nodenumber
-EOF
-
-# Network
+# Firewall
 if (yesno "You want to install the firewall"); then
 cat << EOF >> $scriptdir/install.sh
 # firewall
@@ -133,40 +72,80 @@ ufw status
 EOF
 fi
 
-# Snapshot
-if (yesno "You want to restore a snapshot?"); then
-if [[ $chain = "kusama" ]]
-then
-    snapchain="ksmcc3"
-else
-    snapchain="polkadot"
-fi
+if (yesno "You want to install a service file? Default is warp sync, or you can choose a snapshot"); then
+chain=$(menu "Which chain do you want to install" 0 75 0 polkadot "polkadot node" "kusama" "kusama node" )
+database=$(menu "Which database do you want to install" 0 75 0 paritydb "Paritydb (newer, quicker)" "rocksdb" "Rocksdb (more stable)" )
+#database="paritydb"
+telemetry=$(menu "Which telemetry to use" 0 75 0 "telemetry.polkadot.io" "default telemetry" "telemetry-backend.w3f.community" "1000 validator telemetry" )
+nodedir=$(menu "Where do you want to install" 0 75 0 "/home/polkadot" " best for multi-node install" )
+nodename=$(input "How do you want to name your node. This will be used in the systemctl script and in the homedirectory." 0 70 "mynode" )
+nodenumber=$(menu "Depending on hardware and network it is possible to install extra nodes, official advise is one node per server. For one node choose 01, for the second 02, etc" 0 75 0 "01" "first node" "02" "Second node" "03" "Third node" )
+
+cat << EOF >> $scriptdir/$nodename-$nodenumber.service
+[Unit]
+Description=$chain $nodename-$nodenumber
+After=network.target
+StartLimitIntervalSec=200
+StartLimitBurst=2
+
+[Service]
+ExecStart=/usr/bin/polkadot --chain $chain --name $nodename-$nodenumber --validator --sync warp --state-pruning 1000 --port 303$nodenumber --rpc-port 99$nodenumber --ws-port 98$nodenumber --prometheus-port 96$nodenumber --prometheus-external --base-path $nodedir/$nodename-$nodenumber --database $database --telemetry-url 'wss://$telemetry/submit 1' 
+User=polkadot
+Group=polkadot
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
 cat << EOF >> $scriptdir/install.sh
-# Installing s snapshot
-echo "Restoring a snapshot"
-apt -y install lz4
-curl -o - -L http://snapshot.stakeworld.io/$database-$snapchain.lz4 | lz4 -c -d - | tar -x -C $nodedir/$nodename-$nodenumber/chains/$snapchain
-chown polkadot:polkadot $nodedir/$nodename-$nodenumber/chains/$snapchain -R
+# Install systemctl script
+cp $scriptdir/$nodename-$nodenumber.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable $nodename-$nodenumber
+systemctl start $nodename-$nodenumber
 EOF
 fi
 
-# Start process
+
+# Finish script
 cat << EOF >> $scriptdir/install.sh
-# Start the process
-systemctl start $nodename-$nodenumber
 # Endnote
-echo "If everything is well the node is running now and should be visible from https://$telemetry (tip: start typing to search for your node)"
+echo "If you installed a node it should be running now and visible from telemetry (tip: start typing to search for your node)"
 echo "The script is located in $scriptdir"
 echo "For more information see https://stakeworld.io" 
 EOF
 
-}
 
-function snapinstall {
+# Review the script. Starting an editor seems problematic from piped stdin
+msg "You can review the script and decide to run or edit. Press q to exit the preview, space for next page."
+less $scriptdir/install.sh
+
+if [ -f "$scriptdir/$nodename-$nodenumber.service" ]; then
+less "$scriptdir/$nodename-$nodenumber.service"
+fi
+
+if (yesno "You want to proceed and runt the script? If you want to review first the script can be found in $scriptdir/install.sh"); then
+$scriptdir/install.sh
+fi
+
+echo "Optionally you can restore a snapshot, press any key to continue"
+read 
+
+# Snapshot
+if (yesno "You want to restore a snapshot?"); then
+
+# Start our script
+cat << EOF >> $scriptdir/snapshot.sh
+#!/bin/bash
+# created on $date
+
+EOF
 
 chain=$(menu "Which chain do you want to install" 0 75 0 polkadot "polkadot node" "kusama" "kusama node" )
 database=$(menu "Which database do you want to install" 0 75 0 paritydb "Paritydb (newer, quicker)" "rocksdb" "Rocksdb (more stable)" )
-#database="paritydb"
 snaptargets=`find /home/polkadot /home/polkadot2 /root/.local/share/ -maxdepth 1 -mindepth 1 -type d \( ! -iname ".*" \) -printf "%p -\n" 2> /dev/null`
 nodedir=$(menu "Where do you want to install" 0 75 0 $snaptargets )
 
@@ -177,47 +156,23 @@ else
     snapchain="polkadot"
 fi
 
-# Make our script
-cat << EOF >> $scriptdir/install.sh
-#!/bin/bash
-# created on $date
-
+cat << EOF >> $scriptdir/snapshot.sh
 # Installing s snapshot
+echo "Restoring a snapshot"
+systemctl stop $nodename-$nodenumber 2>/dev/null
 apt -y install lz4
 mkdir -p $nodedir/chains/$snapchain
+rm -fr $nodedir/chains/$snapchain/db 2>/dev/null
+rm -fr $nodedir/chains/$snapchain/paritydb 2>/dev/null
 curl -o - -L http://snapshot.stakeworld.io/$database-$snapchain.lz4 | lz4 -c -d - | tar -x -C $nodedir/chains/$snapchain
 chown polkadot:polkadot $nodedir/chains/$snapchain -R
 EOF
 
-}
-
-# Main logic
-scriptdir="/tmp/node-install"
-if [ -d $scriptdir ] ; then
-	mv /tmp/node-install /tmp/node-install.$(date +%F.%R)
-fi
-	
-mkdir -p $scriptdir
-touch $scriptdir/install.sh
-chmod +x $scriptdir/install.sh
-servername=`hostname`
-date=`date`
-
-mainmenu
-
 # Review the script. Starting an editor seems problematic from piped stdin
 msg "You can review the script and decide to run or edit. Press q to exit the preview, space for next page."
-less $scriptdir/install.sh
+less $scriptdir/snapshot.sh
 
-if [ -f "$scriptdir/$nodename-$nodenumber.service" ]; then
-less "$scriptdir/$nodename-$nodenumber.service"
-read -p "Press any key to continue... " -n1 -s
+if (yesno "You want to proceed and runt the script? If you want to review first the script can be found in $scriptdir/snapshot.sh"); then
+$scriptdir/snapshot.sh
 fi
-
-if (yesno "You want to proceed and runt the script? If you want to review first the script can be found in $scriptdir/install.sh"); then
-$scriptdir/install.sh
 fi
-
-echo "The script is located in $scriptdir"
-echo "For more information see https://stakeworld.io" 
-echo "Script finished"
